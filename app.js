ce6cd2e2692bdfe6784701db9a750ecfc292ce26
Peng Han <peng@Pengs-MacBook-Pro.local>
2021-07-22 20:23:01 -0700

    add bcrypt to the user password

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -26,8 +27,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
@@ -43,23 +42,27 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-  // when save mogon will encript
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
-  })
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    // when save mogon will encript
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    })
+});
 });
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   // when find mogon will decript
   User.findOne({email: username}, function(err,foundUser){
@@ -67,9 +70,11 @@ app.post("/login", function(req, res){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result === true){
+            res.render("secrets");
+          }
+        });
       }
     }
   })
